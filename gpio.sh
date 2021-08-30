fe070d89e4dd8074028791119f6564573d03060b
Bass0315 <1217354870@qq.com>
2021-08-30 11:47:15 +0800

    add gpio.sh/edit main.sh

---
+++ gpio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+chip_gpio=("23" "6" "28" "17" "16" "24" "5" "27" "19" "20")
+
+# config and int gpio	
+for i in $(seq 0 $((${#chip_gpio[*]}-1))) 
+do
+	command="gpio conf GPIO_0 ${chip_gpio[$i]} out"
+    echo ${command} > $(ls /dev/ttyACM*)
+	command="gpio set GPIO_0 ${chip_gpio[$i]} 1"
+	echo ${command} > $(ls /dev/ttyACM*)
+    # do something....
+done
+
+sleep 2
+
+# reset gpio
+for i in $(seq 0 $((${#chip_gpio[*]}-1))) 
+do
+	command="gpio set GPIO_0 ${chip_gpio[$i]} 0"
+	echo ${command} > $(ls /dev/ttyACM*)
+done
--- main.sh
+++ main.sh
@@ -33,23 +33,32 @@ buffer_process=$!
 
 #Erase flash
 echo "flash erase GD25Q16C 0" > $(ls /dev/ttyACM*)
-sleep 0.1
-grep -w "success" temporary_log && displayResult || displayResult
+sleep 0.3
+grep -w "Erase success" temporary_log && displayResult || displayResult
 
 #Write flash
 echo "flash write GD25Q16C 0 0x1234" > $(ls /dev/ttyACM*)
-sleep 0.1
+sleep 0.3
 grep -w "OK" temporary_log && displayResult || displayResult
 
 # Clear buffer
-echo "" > temporary_log
 
-#Read flash
-# echo "flash read GD25Q16C 0 4" > $(ls /dev/ttyACM*)
-# sleep 0.1
-# cat temporary_log
 
-#kill -9 "$buffer_process"
+# Read flash
+echo "flash read GD25Q16C 0 4" > $(ls /dev/ttyACM*)
+sleep 0.1
+grep -w "34 12 00 00" temporary_log && displayResult || displayResult
 
-#echo "flash write GD25Q16C 0  0x1234" > $(ls /dev/ttyACM*)
+
+#echo "" > temporary_log
+kill -9 "$buffer_process"
+
+# Read iic address
+echo "i2c scan I2C_0" > $(ls /dev/ttyACM*)
+sleep 0.1
+cat $(ls /dev/ttyACM*)
+
+./gpio.sh
+
+
 
